16c1cafd3b5ba0428dcb40a8274267bc41b0e77f
Zaraostre <60403347+Zaraostre@users.noreply.github.com>
2020-02-09 13:01:22 +0100

    Classe Nice pour changer les priorités

---
+++ Projet_OS/src/data/processus/Nice.java
@@ -0,0 +1,29 @@
+
+public class Nice {
+
+	private int priority; // between -20 for the highest priority and +19 for the lowest
+	
+	
+
+	public Nice(int priority) throws IllegalArgumentException {
+		if((priority < -20) || (priority > 19)) {
+			throw new IllegalArgumentException();
+		}
+		else {
+		this.priority = priority;
+		}
+	}
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		if((priority < -20) || (priority > 19)) {
+			throw new IllegalArgumentException();
+		}
+		else {
+		this.priority = priority;
+		}
+	}
+
+}
