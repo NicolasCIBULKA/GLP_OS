000f3eb5107534c2e77bab1cf7130b33609cd7c1
Marmeisse <theo.marmeisse@yahoo.fr>
2020-03-10 14:24:36 +0100

    Round robin fonctionnel et ajout de SRTF

--- Projet_OS/src/test/Testscriptexec.java
+++ Projet_OS/src/test/Testscriptexec.java
@@ -29,6 +29,7 @@ public class Testscriptexec {
 		
 		//ProcessusExec procexec = new ProcessusExec(scdriver);
 		String filename = "/Users/theomarmeisse/git/GLP_OS/Projet_OS/src/scripts/convertit_binaire.txt" ;
+		String filename2 = "/Users/theomarmeisse/git/GLP_OS/Projet_OS/src/scripts/chrono.txt";
 		Processus proc2 = new Processus();
 		Processus proc1 = new Processus();
 		Processus proc3 = new Processus();
@@ -36,7 +37,7 @@ public class Testscriptexec {
 		// traduction du langage interpreté en un code utilisable
 		
 		transcriptor.transcription(proc1, filename);
-		transcriptor.transcription(proc2, filename);
+		transcriptor.transcription(proc2, filename2);
 		transcriptor.transcription(proc3, filename);
 		
 		// execution du code via processusexec
@@ -46,15 +47,16 @@ public class Testscriptexec {
 		boy.add(proc1);
 		boy.add(proc2);
 		boy.add(proc3);
-		RoundRobin2 rr = new RoundRobin2();
+		RoundRobin2 rr = new RoundRobin2(scdriver);
 		Interaction inte = new Interaction();
 		
 		
-		rr.runRR(boy, 3,scdriver);
+		rr.runRR(boy, 3);
 		System.out.println(scdriver.getScreencontent());
 		// execution du code via operationexec
-		OperationExec opexec = new OperationExec();
+		//OperationExec opexec = new OperationExec();
 		System.out.println("==================== Execution via Operationexec ====================\n");
+		System.out.println(scdriver.toString());
 		//while(proc.getAlreadydoneoperation() < proc.getNboperation()) {
 			//opexec.operationexecution(proc, proc.getOplist().get(proc.getAlreadydoneoperation()), scdriver);
 			//System.out.println(proc.getAlreadydoneoperation() + " < " + proc.getNboperation());
