c6721167c6efda0853012747d43841822d4dccd7
redouane <redouane@redouane-Aspire-A315-42>
2020-03-23 23:49:30 +0100

    referenced libraries added for the charts

---
+++ Projet_OS/src/gui/Charts.java
@@ -0,0 +1,29 @@
+package gui;
+
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import data.processus.Processus;
+
+public class Charts {
+
+	private ArrayList<Processus> listchart = new ArrayList<Processus>();
+	
+	
+	public JFreeChart getHeightEvolutionChart() {
+		XYSeries serie = new XYSeries("Running programs");
+		for (int index = 0; index < ; index++) {
+			serie.add(index, listchart.);
+		}
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(serie);
+
+		return ChartFactory.createXYLineChart("", "Time", "Number of programs running", dataset, PlotOrientation.VERTICAL, true, true, false);
+	}
+}
