cd7f20e616f4d8e45d6acfb2422380dc00f639ef
Zaraostre <60403347+Zaraostre@users.noreply.github.com>
2020-04-13 15:53:22 +0200

    Chemin

--- Projet_OS/src/data/drivers/HardDiskDriver.java
+++ Projet_OS/src/data/drivers/HardDiskDriver.java
@@ -92,7 +92,7 @@ public class HardDiskDriver extends Driver{
 		   
        try { 
     	   File tempFile = new File(info.getAbsolutePath() + ".tmp");
-    	   BufferedReader br = new BufferedReader(new FileReader("/src/harddisks/info.csv"));
+    	   BufferedReader br = new BufferedReader(new FileReader("./src/harddisks/info.csv"));
     	   writer = new PrintStream(new FileOutputStream(tempFile));
     	   String line = null;
 
@@ -133,7 +133,25 @@ public class HardDiskDriver extends Driver{
 		return hd;
 	}
 	
-
+	public static void main(String[] args) {
+		HardDisk hd = new HardDisk("76");
+		Slot slot1 = new Slot("A");
+		Slot slot2 = new Slot("B");
+		Slot slot3 = new Slot("C");
+		Interaction authorization = new Interaction();
+		HardDiskDriver hdd = new HardDiskDriver("23",authorization,hd);
+		try {
+			hdd.addSlot(slot1);
+			hdd.addSlot(slot2);
+			hdd.addSlot(slot3);
+			hdd.write("Elle a les yeux bleus pour effacer ce ciel gris ","A");
+			hdd.write("Elle a les yeux bleus pour effacer ce ciel fruit ","C");
+			hdd.write("Elle a les yeux bleus pour effacer ce ciel gris ","B");
+			hdd.eraseSlot("B");
+		} catch (FullHDException e) {
+			e.printStackTrace();
+		}
+	}
 
 	
 }
